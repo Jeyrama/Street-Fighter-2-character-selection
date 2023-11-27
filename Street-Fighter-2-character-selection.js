455808fb268e5cd42696ab94c5b6a641fadcb819
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-27 11:07:21 -0800

    Update Street-Fighter-2-character-selection.js

--- Street-Fighter-2-character-selection.js
+++ Street-Fighter-2-character-selection.js
@@ -54,3 +54,22 @@ Examples:
 
 
 // Solution
+
+function streetFighterSelection(fighters, position, moves) {
+  const boundY = fighters.length - 1;
+  const boundX = fighters[0].length - 1;
+
+  let [y, x] = position;
+
+  const controls = {
+    right: () => x === boundX ? x = 0 : x++,
+    left: () => x ? x-- : x = boundX,
+    up: () => y ? y-- : y,
+    down: () => y < boundY ? y++ : y
+  }
+
+  return moves.map(move => {
+    controls[move]();
+    return fighters[y][x];
+  })
+}
